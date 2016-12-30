507b2ce136997d6016b0de524a59db4394ac49b7
giganoide <giganoide@hotmail.com>
2016-12-30 14:09:38 +0100

    Add day 6

---
+++ AdventOfCode/AdventofCode/Day6.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace AdventOfCode
+{
+    public class Day6
+    {
+        public static string GetCode(string messages)
+        {
+            var lines = messages.SplitLines();
+            var code = new StringBuilder();
+            for (var i = 0; i < lines[0].Length; i++)
+                code.Append(GetMostFrequentChar(lines, i));
+            return code.ToString();
+        }
+
+        private static char GetMostFrequentChar(string[] messages, int charIndex)
+        {
+            var builder = new StringBuilder();
+            foreach (var message in messages)
+                builder.Append(message.ToCharArray()[charIndex]);
+            return builder.ToString().GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key;
+        }
+    }
+}
---
+++ AdventOfCode/AdventofCode/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public static class Helper
+    {
+        public static string[] SplitLines(this string input)
+        {
+            return input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+        }
+    }
+}
